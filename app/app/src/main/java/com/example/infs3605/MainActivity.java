6eb93edf4c78c4263874f693c3ed5fc7ebc40323
GrantyLiu <57003587+GrantyLiu@users.noreply.github.com>
2020-06-23 23:16:58 +1000

    test321
    asdas

--- app/app/src/main/java/com/example/infs3605/MainActivity.java
+++ app/app/src/main/java/com/example/infs3605/MainActivity.java
@@ -5,7 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//LEwes sucks dick
+    //yummy
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
